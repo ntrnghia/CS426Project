4a1d9bc6458e5462560e49ac229d99d533d3efc2
ntrnghia24 <kudoshinichiconan1998@gmail.com>
2018-07-18 10:34:31 +0700

    Back to initial

--- app/src/main/java/com/example/kudos/miniproject1/Add_Change_Cinema.java
+++ app/src/main/java/com/example/kudos/miniproject1/Add_Change_Cinema.java
@@ -32,7 +32,6 @@ public class Add_Change_Cinema extends AppCompatActivity {
     Cinema cinema;
     Bitmap bitmap;
     Boolean is_change;
-    //hehehehe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
